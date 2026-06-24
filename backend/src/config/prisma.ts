318a031e2a54e38598bdefbb79929686a2f3e9e3
ramuchidu03@gmail.com <ramuchidu03@gmail.com>
2026-06-24 22:01:24 +0530

    cleared the issue for deployment

--- backend/src/config/prisma.ts
+++ backend/src/config/prisma.ts
@@ -1,13 +1,19 @@
 import { PrismaClient } from "@prisma/client";
+import { PrismaBetterSqlite3 } from "@prisma/adapter-better-sqlite3";
 import { env } from "./env";
 
 declare global {
   var prisma: PrismaClient | undefined;
 }
 
+const adapter = new PrismaBetterSqlite3({
+  url: "file:./dev.db"
+});
+
 const prisma =
   global.prisma ||
   new PrismaClient({
+    adapter,
     log: env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
   });
 
